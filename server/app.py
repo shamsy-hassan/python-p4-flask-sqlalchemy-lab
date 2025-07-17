1a6e7db0e70af13101612759f03722eee6e0bc47
shamsy-hassan <shamsyhassan254@gmail.com>
2025-07-17 21:53:59 +0300

    flask

--- server/app.py
+++ server/app.py
@@ -19,15 +19,44 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
+    if animal:
+        return f'''
+            <ul>Name: {animal.name}</ul>
+            <ul>Species: {animal.species}</ul>
+            <ul>Zookeeper: {animal.zookeeper.name}</ul>
+            <ul>Enclosure: {animal.enclosure.environment}</ul>
+        '''
+    return make_response('<h1>Animal not found</h1>', 404)
+
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+    if zookeeper:
+        animals_list = ''.join(
+            [f'<li>{animal.name}</li>' for animal in zookeeper.animals]
+        )
+        return f'''
+            <ul>Name: {zookeeper.name}</ul>
+            <ul>Birthday: {zookeeper.birthday}</ul>
+            <ul>Animal: <ul>{animals_list}</ul></ul>
+        '''
+    return make_response('<h1>Zookeeper not found</h1>', 404)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.get(id)
+    if enclosure:
+        animals_list = ''.join(
+            [f'<li>{animal.name}</li>' for animal in enclosure.animals]
+        )
+        return f'''
+            <ul>Environment: {enclosure.environment}</ul>
+            <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+            <ul>Animal: <ul>{animals_list}</ul></ul>
+        '''
+    return make_response('<h1>Enclosure not found</h1>', 404)
 
 
 if __name__ == '__main__':
